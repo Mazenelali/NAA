d7433098ba464df4117f4417574b471378d08ffc
mazen <mazenelali105@gmail.com>
2025-02-23 16:35:11 +0200

    fixe

--- src/components/CommingSoon.js
+++ src/components/CommingSoon.js
@@ -3,23 +3,31 @@ import balckLogo from "../assets/Black.png"
 import './CommingSoon.css'
 
 export default function CommingSoon() {
-    const [time , setTime] = useState(null)
+    const [time, setTime] = useState(null)
 
 
-    const targetDate = new Date("Apr 5, 2025 15:37:25").getTime()
+    const targetDate = new Date("Mar 15, 2025 15:37:25").getTime()
 
     setInterval(() => {
         const currentDate = new Date().getTime()
-        const dif = targetDate - currentDate  
-    
+        const dif = targetDate - currentDate
+
         var days = Math.floor(dif / (1000 * 60 * 60 * 24));
         var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((dif % (1000 * 60)) / 1000);
 
-            setTime(`${days} days ${hours}:${minutes}:${seconds}`)
+        setTime(`${days} days ${hours}:${minutes}:${seconds}`)
 
     }, 1000);
+
+    const loadingContainer = document.querySelector('.loading-dots');
+
+    const newDot = document.createElement('span');
+    newDot.classList.add('dot');
+    if(!loadingContainer)
+    loadingContainer?.appendChild(newDot);
+
 
 
 
@@ -27,7 +35,14 @@ export default function CommingSoon() {
         <main className="container">
             <div className="inner">
                 <img src={balckLogo} alt="logo" className="logo" />
-                <p className="title"> Comming soon ... </p>
+                <div style={{ display: 'flex' , alignItems:'center' ,  gap:'20px' }}>
+                    <p className="title"> Launching Soon  </p>
+                    <div class="loading-dots">
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+                </div>
                 <p className="timer"> {time} </p>
                 <button className="button" > Check our menu</button>
             </div>
