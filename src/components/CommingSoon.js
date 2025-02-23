2579907b4bd8789e288cc5e5ffc9ab81aed75f29
mazen <mazenelali105@gmail.com>
2025-02-23 16:05:27 +0200

    replace

---
+++ src/components/CommingSoon.js
@@ -0,0 +1,36 @@
+import { useState } from "react"
+import balckLogo from "../assets/Black.png"
+import './CommingSoon.css'
+
+export default function CommingSoon() {
+    const [time , setTime] = useState(null)
+
+
+    const targetDate = new Date("Apr 5, 2025 15:37:25").getTime()
+
+    setInterval(() => {
+        const currentDate = new Date().getTime()
+        const dif = targetDate - currentDate  
+    
+        var days = Math.floor(dif / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((dif % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((dif % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((dif % (1000 * 60)) / 1000);
+
+            setTime(`${days} days ${hours}:${minutes}:${seconds}`)
+
+    }, 1000);
+
+
+
+    return (
+        <main className="container">
+            <div className="inner">
+                <img src={balckLogo} alt="logo" className="logo" />
+                <p className="title"> Comming soon ... </p>
+                <p className="timer"> {time} </p>
+                <button className="button" > Check our menu</button>
+            </div>
+        </main>
+    )
+}
